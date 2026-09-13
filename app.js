d683dbe376093d8c93b9796fc625d61539b84453
mcastellone <167670632+mcastellone@users.noreply.github.com>
2026-09-12 19:16:38 -0700

    Add original Travlr root project files

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+require('./app_api/models/db');
+
+var passport = require('passport');
+require('./app_api/config/passport');
+
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./routes/users');
+var apiRouter = require('./app_api/routes/index');
+
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./routes/users');
+var apiRouter = require('./app_api/routes/index');
+
+var app = express();
+
+app.use(cors());
+
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(passport.initialize());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+
+// catch 404
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
